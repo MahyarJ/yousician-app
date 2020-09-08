cc0fa97806c50d21c4b8cb7b2fb7793092144a5b
Mahyar Jamalabadi <mahyar.jamalabadi@gmail.com>
2020-09-08 17:00:33 +0430

    Add color palette to styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/fonts.module.sass';
-import './styles/global.module.sass';
+import './styles/fonts.sass';
+import './styles/global.sass';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
