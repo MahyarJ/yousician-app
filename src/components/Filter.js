9bf58abb9be10452ce127cfa26b293a84e77e35b
Mahyar Jamalabadi <mahyar.jamalabadi@gmail.com>
2020-09-08 13:57:46 +0430

    Implement level filter selection

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 import styles from './Filter.module.sass';
 import LevelIndicator from './LevelIndicator';
+import { uniq } from 'lodash';
 
 const filterMap = new Array(15).fill(0);
 
 const Filter = (props) => {
   const [isOpen, setisOpen] = useState(false);
+  const [selected, setselected] = useState([]);
+
   const toggleFilter = () => {
     setisOpen(!isOpen);
   };
+
+  const handleSelectLevel = (level) => {
+    console.log(selected);
+    if (selected.includes(level)) setselected(selected.filter((s) => s !== level));
+    else setselected(uniq(selected.concat(level)));
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.filterButton} onClick={toggleFilter}>
@@ -18,7 +28,14 @@ const Filter = (props) => {
       {isOpen && (
         <div className={styles.levelPanel}>
           {filterMap.map((_, index) => {
-            return <LevelIndicator key={index + 1} value={index + 1} />;
+            return (
+              <LevelIndicator
+                key={index + 1}
+                value={index + 1}
+                isSelected={selected.includes(index + 1)}
+                onSelect={handleSelectLevel}
+              />
+            );
           })}
         </div>
       )}
--- src/components/LevelIndicator.js
+++ src/components/LevelIndicator.js
@@ -3,7 +3,7 @@ import styles from './LevelIndicator.module.sass';
 import classNames from 'classnames/bind';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const LevelIndicator = ({ value }) => {
+const LevelIndicator = ({ value, isSelected, onSelect }) => {
   const cx = classNames.bind(styles);
   const graphStyle = cx({
     levelGraph: true,
@@ -12,18 +12,41 @@ const LevelIndicator = ({ value }) => {
     highLevel: 10 < value && value <= 15,
   });
 
+  const labelStyle = cx({
+    label: true,
+    selectedLabel: isSelected,
+  });
+
+  const levelStyle = cx({
+    level: true,
+    selectedLevel: isSelected,
+  });
+
+  const handleSelect = () => {
+    if (!onSelect) return;
+    onSelect(value);
+  };
+
   return (
-    <div className={styles.level}>
-      <CircularProgress className={styles.underlyingGraph} variant="static" value={100} />
-      <CircularProgress
-        className={graphStyle}
-        variant="static"
-        value={(value * 100) / 15}
-      />
-      <div className={styles.separator0} />
-      <div className={styles.separator1} />
-      <div className={styles.separator2} />
-      <p className={styles.levelValue}>{value}</p>
+    <div className={levelStyle} onClick={handleSelect}>
+      {!isSelected && (
+        <>
+          <CircularProgress
+            className={styles.underlyingGraph}
+            variant="static"
+            value={100}
+          />
+          <CircularProgress
+            className={graphStyle}
+            variant="static"
+            value={(value * 100) / 15}
+          />
+          <div className={styles.separator0} />
+          <div className={styles.separator1} />
+          <div className={styles.separator2} />
+        </>
+      )}
+      <p className={labelStyle}>{value}</p>
     </div>
   );
 };
